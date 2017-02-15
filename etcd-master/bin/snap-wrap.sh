e3a902a09090538befd8eb7a5059adc35bcc5cf2
Mark Shuttleworth <mark@ubuntu.com>
2017-02-15 12:31:06 +0000

    Remove old buildcmd plugin from etcd-master snapcraft.yaml

--- etcd-master/bin/snap-wrap.sh
+++ etcd-master/bin/snap-wrap.sh
@@ -4,26 +4,36 @@
 
 # Use the system writable dir if run as root
 if [[ $EUID -ne 0 ]]; then
-  TARGET_DATA_DIR=$SNAP_USER_DATA/data
-  TARGET_CONF=$SNAP_USER_DATA/etcd.conf.yml
+  DATA_DIR=$SNAP_USER_DATA
+  CONF_DIR=$SNAP_USER_COMMON
+  echo "Running as user with data in $DATA_DIR"
 else
-  TARGET_DATA_DIR=$SNAP_DATA/data
-  TARGET_CONF=$SNAP_DATA/etcd.conf.yml
+  DATA_DIR=$SNAP_DATA
+  CONF_DIR=$SNAP_COMMON
+  echo "Running as system with data in $DATA_DIR"
 fi
 
+# Check for a version 2.x config and bail if so
+if [ -e $CONF_DIR/etcd.conf ]; then
+  echo "etcd 3.x is compatible with etcd 3.0 but not 2.x."
+  echo
+  echo "It appears you have an existing etcd 2.x configuration in "
+  echo "$CONF_DIR/etcd.conf which means you need to switch "
+  echo "to the 3.0 channel of etcd in order to upgrade to 3.0 "
+  echo "before switching back and trying to run this etcd 3.1."
+  exit 1
+fi
+
+# See if there is a configuration file
+TARGET_CONF=$CONF_DIR/etcd.conf.yml
 if [ -e $TARGET_CONF ]; then
-  # The desired configuration file is already in place
-  echo "Configuration exists"
+  echo "Configuration from $TARGET_CONF"
 else
-  cp $SNAP/etcd.conf.yml.sample $TARGET_CONF
-  echo "Creating fresh config at $TARGET_CONF"
+  echo "No config found, please create one at $TARGET_CONF"
+  echo "See $SNAP/etcd.conf.yml.sample for an example."
+  exit 0
 fi
 
-# Only set the environment if the user has not done so
-ETCD_DATA_DIR="${ETCD_DATA_DIR:=$TARGET_DATA_DIR}"
-
-# Make sure it is exported not just set
-export ETCD_DATA_DIR
-
-exec $SNAP/bin/etcd "$@"
+# Launch with the default config file
+exec $SNAP/bin/etcd --config-file $TARGET_CONF "$@"
 
